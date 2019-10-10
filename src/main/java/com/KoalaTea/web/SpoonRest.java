31cd14a27af9f08f9bce698b0fe40795c67e11c0
th3alchemist_0 <ayo.j.arum@gmail.com>
2019-10-10 15:50:29 -0500

    added recipe endpoint

--- src/main/java/com/KoalaTea/web/SpoonRest.java
+++ src/main/java/com/KoalaTea/web/SpoonRest.java
@@ -1,8 +1,14 @@
 package com.KoalaTea.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.client.RestTemplate;
 
 import com.KoalaTea.model.restModel.Joke;
@@ -11,6 +17,7 @@ import com.KoalaTea.model.restModel.SpoonacularIngredient;
 import com.KoalaTea.model.restModel.SpoonacularRecipe;
 import com.KoalaTea.model.restModel.SpoonacularRecipeResponse;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/spoon")
 public class SpoonRest {
@@ -42,14 +49,26 @@ public class SpoonRest {
 		SpoonacularIngredient retrievedIngredient = restTemplate.getForObject(url, SpoonacularIngredient.class);
 		return retrievedIngredient;
 	}
-
-	@GetMapping(value="/recipe")
-	public static SpoonacularRecipe getSpoonRecipe() {
-		String url = "https://api.spoonacular.com/recipes/search?limit=1&apiKey="
-				     + System.getenv("SPOON_API_KEY");
+	
+	@PostMapping(value="/recipe")
+	public static SpoonacularRecipe getSpoonRecipe(@RequestBody String query) {
+		String url = "https://api.spoonacular.com/recipes/search?apiKey="
+				     + System.getenv("SPOON_API_KEY")
+				     + "&" + query;
+		
+		System.err.println(url);
 
 		RestTemplate restTemplate = new RestTemplate();
 		SpoonacularRecipeResponse retrievedRecipe = restTemplate.getForObject(url, SpoonacularRecipeResponse.class);
-		return retrievedRecipe.getResults().get(0);
+		
+		System.err.println(retrievedRecipe);
+		
+		if(retrievedRecipe.getResults().size() == 0) {
+			SpoonacularRecipe rtn =  new SpoonacularRecipe();
+			rtn.setTitle("No recipe found.");
+			return rtn;
+		}
+		else
+			return retrievedRecipe.getResults().get(0);
 	}
 }
